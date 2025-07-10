e4d2b2eb73d41e169b35a6ddaa3dbd99cbcc2d3e
Parthiban D <parthiband2020@gmail.com>
2025-07-10 11:21:03 +0530

    Data ingestion and validation completed

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.DSProject_Winequality import logger
+from src.DSProject_Winequality.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.DSProject_Winequality.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
-logger.info("welcome to our custom logging")
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data_Validation_Stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        data_validation=DataValidationTrainingPipeline()
+        data_validation.initiate_data_validation()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
